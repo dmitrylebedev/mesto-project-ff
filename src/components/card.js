0f4c621a36d17f9e0309e9ff3406dca813239966
dmitrylebedev <dimitrylebedev@yandex.ru>
2025-11-08 13:55:37 +0300

    Добавляет отображение количества лайков карточки

--- src/components/card.js
+++ src/components/card.js
@@ -7,10 +7,16 @@ export function createCard(cardData, deleteCard, likeCard, openImageModal) {
   const cardTitle = cardRoot.querySelector('.card__title');
   const deleteButton = cardRoot.querySelector('.card__delete-button');
   const likeButton = cardRoot.querySelector('.card__like-button');
+  const likeCount = cardRoot.querySelector('.card__like-count');
 
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
+  
+  // Отображаем количество лайков
+  if (likeCount && cardData.likes) {
+    likeCount.textContent = cardData.likes.length;
+  }
 
   cardImage.addEventListener('click', () => {
     openImageModal(cardData);
